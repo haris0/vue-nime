0de4e9997fa818f088101d0bea51e3f97bf71b0d
haris0 <hari.surrisyad@gmail.com>
2019-09-25 21:52:57 +0700

    cleaning base project

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+
+  let self;
+
+  export default {
+    name: 'Homepage',
+    created(){
+      self = this;  
+    },
+    mounted(){
+    },
+    data:()=>({
+    }),
+    computed: {
+     
+    },
+    methods:{
+      
+    }
+  }
+
+</script>
+
+<style scoped>
+  
+</style>
