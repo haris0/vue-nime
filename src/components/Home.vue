9f8256e03fad95c659b6ec6a60df58ea033ab970
haris0 <hari.surrisyad@gmail.com>
2019-09-28 00:09:21 +0700

    home page view

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="container">
-    
+    <div class="header">
+      <div class="search-title">
+        <span>
+          Nusanime
+        </span>
+      </div> 
+    </div>
+    <div class="con-search">
+      <div class="search">
+        <a-input-search
+          size="large"
+          placeholder="input search text"
+          @search="onSearch"
+        />
+      </div>
+    </div>
+    <div class="sesion">
+      <a-row style="top: 50px;">
+        <a-col :xs="12" :xl="6" style="padding:10px" v-for="ses in session" v-bind:key="ses.ses">
+          <a-card hoverable class="card-ses">
+            <div slot="cover" :style="'height :100px;background:'+ses.color">
+
+            </div>
+            <a-card-meta
+              :title="ses.ses">
+              <template slot="description">{{ses.year}}</template>
+            </a-card-meta>
+          </a-card>
+        </a-col>
+      </a-row>
+    </div>
   </div>
 </template>
 
@@ -16,17 +46,75 @@
     mounted(){
     },
     data:()=>({
+      session : [
+        {
+          ses : "Winter",
+          year: 2019,
+          color : "#535c68"
+        },
+        {
+          ses : "Spring",
+          year: 2019,
+          color : "#22a6b3"
+        },
+        {
+          ses : "Summer",
+          year: 2019,
+          color : "#eb4d4b"
+        },
+        {
+          ses : "Fall",
+          year: 2019,
+          color : "#f9ca24"
+        }
+      ]
     }),
     computed: {
      
     },
     methods:{
-      
+      onSearch: function(){
+
+      }
     }
   }
 
 </script>
 
 <style scoped>
-  
+  .container{
+    height:100vh;
+  }
+  .header{
+    height: 40%;
+    width: 100%;
+    line-height: 4.5;
+    background-image: url("../assets/background.jpg");
+    background-position: center;
+    background-size: cover;
+  }
+  .con-search{
+    margin-top: -20px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+  }
+  .search{
+    width: 70%;
+  }
+  .search-title{
+    text-align: center;
+    font-size: 50px;
+    color:white;
+  }
+  .sesion{
+    text-align: center;
+    margin: 0 20px;
+    margin-bottom: 70px;
+  }
+  .footer{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {  Input,
+          Col,
+          Row,
+          Card} from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css'
 
+Vue.use(Input);
+Vue.use(Col);
+Vue.use(Row);
+Vue.use(Card);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
